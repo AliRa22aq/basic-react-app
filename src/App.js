92e5b65de2ed99743db27e5eaa46a5e2350db5f2
Ali Razzaq <alirazzaquet@gmail.com>
2020-08-12 01:54:23 +0500

    Update App.js
    Updated App.js with Name

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <br>
+          By Ali Razzaq
         </p>
         <a
           className="App-link"
