a9e2c17656f78bf35285b88037c21759d1f02794
Ali Razzaq <alirazzaquet@gmail.com>
2020-08-12 21:26:28 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import './Hello.css'
 
 function App(props) {
   return <div className="c"> Hello World from {props.name} 
-  <Hello firstname="Baoooooo"/>
-  <Hello2 x="hahahaha" y={5-10}/>
+  <Hello firstname="Ali"/>
+  <Hello2 x="PIAIC" y={5-10}/>
   </div> 
 }
 
